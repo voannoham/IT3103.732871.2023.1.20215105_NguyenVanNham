68649667382f86334ed0adbe26592a0747cfb97e
Nguyen Van Nham <nhampmq@gmail.com>
2023-11-23 09:50:40 +0700

    Working with method overloading

--- AimsProject/src/oopLab2/Cart.java
+++ AimsProject/src/oopLab2/Cart.java
@@ -19,6 +19,38 @@ public class Cart {
       }
 	}
 	
+	//(Nguyen Van Nham 20215105-lab3 Cart.java)
+	//Overloading by differing types of parameter
+	//add an array item to the list
+	public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+		if (qtyOrdered + dvdList.length > itemsOrdered.length) {//check if the cart is not empty enough to add array of item
+			System.out.printf("NguyenVanNham20215105-The cart is not empty enough to add array item");
+		}
+		else {
+			for (DigitalVideoDisc disc: dvdList) {
+				itemsOrdered[qtyOrdered] = disc;//add disc
+				qtyOrdered++;//update number of dvd in the cart
+				System.out.printf("NguyenVanNham20215105-The disc %s has been added.\n",disc.getTitle());
+			}
+		}
+	}
+		
+	//Overloading by differing the number of parameters
+	//add two item to the list
+	public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		if (qtyOrdered + 2 > itemsOrdered.length) {//check if the cart is not empty enough  to add 2 items
+			System.out.println("NguyenVanNham20215105-The cart is not empty enough to add 2 items.");
+		} 
+		else {
+			itemsOrdered[qtyOrdered] = dvd1;//add disc1
+			qtyOrdered++;//update number of DVD in the cart
+			System.out.printf("NguyenVanNham20215105-The disc %s has been added.\n",dvd1.getTitle());
+		    itemsOrdered[qtyOrdered] = dvd2;//add disc2
+		    qtyOrdered++;//update number of dvd in the cart
+		    System.out.printf("NguyenVanNham20215105-The disc %s has been added.\n",dvd2.getTitle());
+		}
+	}
+		
 	//remove the item passed by argument from the list
 	public void removeDigitalVideoDisc(DigitalVideoDisc disc){
 		for(int i = 0; i < qtyOrdered; i++){//loop all DVD in the cart
