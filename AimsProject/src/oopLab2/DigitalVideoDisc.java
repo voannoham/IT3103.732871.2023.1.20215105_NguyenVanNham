c960a2ed9b57a09db854a11b82b89e2958a35034
Nguyen Van Nham <nhampmq@gmail.com>
2023-11-27 15:00:07 +0700

    print the list of ordered items of a cart

--- AimsProject/src/oopLab2/DigitalVideoDisc.java
+++ AimsProject/src/oopLab2/DigitalVideoDisc.java
@@ -79,24 +79,4 @@ public class DigitalVideoDisc{
 	public static int getNbDigitalVideoDiscs(){
 		return nbDigitalVideoDiscs;
 	}
-	
-	// check if the corresponding disk is a match given the ID
-	public boolean isMath(int id) {
-		if (this.id == id) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	// check if the corresponding disk is a match given the title
-		public boolean isMath(String title) {
-			if (this.title == title) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
 }
